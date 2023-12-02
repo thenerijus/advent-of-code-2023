cfbb6c847302a66548f5091c775fa24852e0a0fb
Nerijus <nerijus.bucinskis@gmail.com>
2023-12-02 10:45:04 +0200

    Day 1 part one solved

--- Day01_1.java
+++ Day01_1.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Part one of
  * <a href="https://adventofcode.com/2023/day/1">Day one</a>
@@ -6,10 +10,16 @@
  */
 public class Day01_1 {
     public static void main(String[] args) {
-        System.out.println(": " + new Day01_1().getResult());
+        System.out.println("Sum of all of the calibration values: " + new Day01_1().getResult());
     }
 
     private int getResult() {
-        return -1;
+        List<String> strings = Inputs.readStrings("Day01");
+        return strings.stream()
+                .map(s -> Arrays.stream(s.split(""))
+                        .filter(c -> c.matches("\\d"))
+                        .collect(Collectors.toList()))
+                .mapToInt(numbers -> Integer.parseInt("" + numbers.get(0) + numbers.get(numbers.size() - 1)))
+                .sum();
     }
 }
--- Day02_1.java
+++ Day02_1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Part two of
+ * Part one of
  * <a href="https://adventofcode.com/2023/day/2">Day two</a>
  *
  * @author Nerijus
