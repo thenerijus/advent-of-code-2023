4e0cdeb60ad0e3e6b464eaea235db2d390bc0c2f
Nerijus <nerijus.bucinskis@gmail.com>
2023-12-02 11:14:49 +0200

    Day 1 part two solved

--- Day01_2.java
+++ Day01_2.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Part two of
  * <a href="https://adventofcode.com/2023/day/1">Day one</a>
@@ -5,11 +11,42 @@
  * @author Nerijus
  */
 public class Day01_2 extends Day01_1 {
+    private static final Map<String, Integer> NUMBER_MAP = Map.of(
+            "one", 1,
+            "two", 2,
+            "three", 3,
+            "four", 4,
+            "five", 5,
+            "six", 6,
+            "seven", 7,
+            "eight", 8,
+            "nine", 9);
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("(?<=(one|two|three|four|five|six|seven|eight|nine|[1-9]))");
+
+
     public static void main(String[] args) {
-        System.out.println(": " + new Day01_2().getResult());
+        System.out.println("Sum of all of the calibration values: " + new Day01_2().getResult());
     }
 
     private int getResult() {
-        return -1;
+        List<String> strings = Inputs.readStrings("Day01");
+        return strings.stream()
+                .map(this::extractNumbers)
+                .mapToInt(numbers -> Integer.parseInt("" + numbers.get(0) + numbers.get(numbers.size() - 1)))
+                .sum();
+    }
+
+    private List<Integer> extractNumbers(String inputLine) {
+        Matcher matcher = NUMBER_PATTERN.matcher(inputLine);
+        List<Integer> numbers = new ArrayList<>();
+        while (matcher.find()) {
+            numbers.add(toNumber(matcher.group(1)));
+        }
+        return numbers;
+    }
+
+    int toNumber(String stringNumber) {
+        Integer mapped = NUMBER_MAP.get(stringNumber);
+        return mapped != null ? mapped : Integer.parseInt(stringNumber);
     }
 }
