3a277340795d04d75a423eecb8d4e2835eae9bfa
Nerijus <nerijus.bucinskis@gmail.com>
2023-12-02 11:17:02 +0200

    Day 3 placeholder

---
+++ Day03_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * <a href="https://adventofcode.com/2023/day/3">Day three</a>
+ *
+ * @author Nerijus
+ */
+public class Day03_1 {
+    public static void main(String[] args) {
+        System.out.println(": " + new Day03_1().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
