23d53374aedb256e306e493ce33f6e86405dd3b6
Nerijus <nerijus.bucinskis@gmail.com>
2023-12-03 14:41:23 +0200

    Day 4 placeholder

---
+++ Day04_1.java
@@ -0,0 +1,15 @@
+/**
+ * Part one of
+ * <a href="https://adventofcode.com/2023/day/4">Day four</a>
+ *
+ * @author Nerijus
+ */
+public class Day04_1 {
+    public static void main(String[] args) {
+        System.out.println(": " + new Day04_1().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
---
+++ Day04_2.java
@@ -0,0 +1,15 @@
+/**
+ * Part two of
+ * <a href="https://adventofcode.com/2023/day/4">Day four</a>
+ *
+ * @author Nerijus
+ */
+public class Day04_2 extends Day04_1 {
+    public static void main(String[] args) {
+        System.out.println(": " + new Day04_2().getResult());
+    }
+
+    private int getResult() {
+        return -1;
+    }
+}
