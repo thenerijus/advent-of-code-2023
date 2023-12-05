d6d0e9273531385be3d5691f3cd15e4602b8ff0d
Nerijus <nerijus.bucinskis@gmail.com>
2023-12-05 22:10:41 +0200

    Day 5 part 1 solved

--- Day05_1.java
+++ Day05_1.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Part one of
  * <a href="https://adventofcode.com/2023/day/5">Day five</a>
@@ -6,10 +11,62 @@
  */
 public class Day05_1 {
     public static void main(String[] args) {
-        System.out.println(": " + new Day05_1().getResult());
+        System.out.println("Lowest location number: " + new Day05_1().getResult());
     }
 
-    private int getResult() {
-        return -1;
+    private long getResult() {
+        String instructions = Inputs.readString("Day05");
+        String[] parts = instructions.split("\n\n");
+        List<Long> seeds = Arrays.stream(parts[0].split(": ")[1].split(" ")).map(Long::parseLong).toList();
+
+        LinkedList<MappingGroup> groups = new LinkedList<>();
+        for (int i = 1; i < parts.length; i++) {
+            groups.add(new MappingGroup(parts[i]));
+        }
+
+        return seeds.stream().mapToLong(s -> getDestination(s, groups)).min().orElseThrow();
+    }
+
+    private long getDestination(Long seed, LinkedList<MappingGroup> groups) {
+        long destination = seed;
+        for (MappingGroup group : groups) {
+            destination = group.getDestination(destination);
+        }
+        return destination;
+    }
+
+    static class MappingGroup {
+        List<Range> ranges;
+
+        MappingGroup(String source) {
+            ranges = Arrays.stream(source.split("\n")).skip(1).map(Range::new).collect(Collectors.toList());
+        }
+
+        long getDestination(long input) {
+            Range matchingRange = ranges.stream().filter(r -> r.matches(input)).findFirst().orElse(null);
+            return matchingRange != null ? matchingRange.getDestination(input) : input;
+        }
+    }
+
+    static class Range {
+        long destinationRangeStart;
+        long sourceRangeStart;
+        long rangeLength;
+
+        Range(String source) {
+            String[] parts = source.split(" ");
+            destinationRangeStart = Long.parseLong(parts[0]);
+            sourceRangeStart = Long.parseLong(parts[1]);
+            rangeLength = Long.parseLong(parts[2]);
+        }
+
+        boolean matches(long input) {
+            return input >= sourceRangeStart &&
+                    input <= sourceRangeStart + rangeLength;
+        }
+
+        long getDestination(long input) {
+            return destinationRangeStart + (input - sourceRangeStart);
+        }
     }
 }
